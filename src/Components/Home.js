14133a3cca818bd01209827b0d506d44af91569d
Rishabh <rishabh.saxena2002@gmail.com>
2023-01-02 20:34:25 +0530

    added inline styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -249,7 +249,7 @@ const Home = () => {
                       <button
                           className="btn btn-primary"
                         >
-                          <a href={`mailto:${value.email}?subject=Regarding ${value.subject}&body=Dear ${value.name}`}>Reply</a>
+                          <a href={`mailto:${value.email}?subject=Regarding ${value.subject}&body=Dear ${value.name}`} style={{textDecoration:"none", color:"white"}}>Reply</a>
                         </button>
                         <button
                           onClick={() => {
